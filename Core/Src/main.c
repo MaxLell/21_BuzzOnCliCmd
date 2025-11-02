7bf242454ba37e6c1e93616fc05527713e2dab2a
maxkoell <maxkoell@proton.me>
2025-11-02 17:00:04 +0100

    Updated the Pinout, since the one that is mentioned on the click shield is not accurate

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,7 +20,6 @@
 #include "main.h"
 #include "icache.h"
 #include "tim.h"
-#include "usart.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -95,8 +94,7 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_ICACHE_Init();
-  MX_TIM3_Init();
-  MX_USART1_UART_Init();
+  MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
   user_main();
   /* USER CODE END 2 */
